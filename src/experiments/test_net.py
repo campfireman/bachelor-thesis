a3cc8b2187941558bc8adc7ef6716cad2f3b4fa2
Ture Claussen <ture.cl.business@gmail.com>
2021-11-07 23:14:28 +0100

    Test preliminary NN

---
+++ src/experiments/test_net.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append('..')
+sys.path.append('../..')
+
+from src.Abalone import AbaloneGame
+from src.AbaloneNN import AbaloneNN
+
+nn = AbaloneNN(AbaloneGame(), {
+    'lr': 0.001,
+    'dropout': 0.3,
+    'epochs': 10,
+    'batch_size': 64,
+    'cuda': False,
+    'num_channels': 512,
+})
+
+nn.show_info()
