1eb0acda7d05d2de3eaa1d1c749dcb4b36b556c6
Ture Claussen <ture.cl.business@gmail.com>
2021-11-09 12:03:25 +0100

    Implement game interface

--- src/Abalone.py
+++ src/Abalone.py
@@ -1,4 +1,12 @@
-class AbaloneGame():
+import numpy as np
+from abalone_engine.game import Game as Engine
+from abalone_engine.game import Move
+from alpha_zero_general import Game
+
+from .utils import move_index_to_standard, move_standard_to_index
+
+
+class AbaloneGame(Game):
     """
     This class specifies the base Game class. To define your own game, subclass
     this class and implement the functions below. This works when the game is
@@ -10,7 +18,7 @@ class AbaloneGame():
     """
 
     def __init__(self):
-        pass
+        self.engine = Engine
 
     def getInitBoard(self):
         """
@@ -18,7 +26,18 @@ class AbaloneGame():
             startBoard: a representation of the board (ideally this is the form
                         that will be the input to your neural network)
         """
-        return
+        return [
+            # 0  1  2  3  4  5  6  7  8
+            [0, 0, 0, 0, -1, -1, -1, -1, -1],  # 0
+            [0, 0, 0, -1, -1, -1, -1, -1, -1],  # 1
+            [0, 0, 0, 0, -1, -1, -1, 0, 0],  # 2
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 3
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 4
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 5
+            [0, 0, 1, 1, 1, 0, 0, 0, 0],  # 6
+            [1, 1, 1, 1, 1, 1, 0, 0, 0],  # 7
+            [1, 1, 1, 1, 1, 0, 0, 0, 0],  # 8
+        ]
 
     def getBoardSize(self):
         """
@@ -45,7 +64,9 @@ class AbaloneGame():
             nextBoard: board after applying action
             nextPlayer: player who plays in the next turn (should be -player)
         """
-        pass
+        game = self.engine.from_array(board, player)
+        game.standard_move(move_index_to_standard(action))
+        return game.to_array()
 
     def getValidMoves(self, board, player):
         """
@@ -58,7 +79,13 @@ class AbaloneGame():
                         moves that are valid from the current board and player,
                         0 for invalid moves
         """
-        pass
+        game = self.engine.from_array(board, player)
+        moves = np.zeros(self.getActionSize(), dtype=np.float32)
+        for move in game.generate_legal_moves():
+            index = move_standard_to_index(
+                Move.from_original(move).to_standard())
+            moves[index] = 1
+        return moves
 
     def getGameEnded(self, board, player):
         """
@@ -71,7 +98,10 @@ class AbaloneGame():
                small non-zero value for draw.
 
         """
-        pass
+        game = self.engine.from_array(board, player)
+        if game.is_over():
+            return 1 if game.get_winner().value == player else -1
+        return 0
 
     def getCanonicalForm(self, board, player):
         """
@@ -87,7 +117,7 @@ class AbaloneGame():
                             board as is. When the player is black, we can invert
                             the colors and return the board.
         """
-        pass
+        return self.engine.from_array(board, player).canonical_board()
 
     def getSymmetries(self, board, pi):
         """
@@ -100,7 +130,7 @@ class AbaloneGame():
                        form of the board and the corresponding pi vector. This
                        is used when training the neural network from examples.
         """
-        pass
+        return [(board, pi)]
 
     def stringRepresentation(self, board):
         """
@@ -111,4 +141,7 @@ class AbaloneGame():
             boardString: a quick conversion of board to a string format.
                          Required by MCTS for hashing.
         """
-        pass
+        string = ''
+        for line in board:
+            string.append(''.join(line))
+        return string
