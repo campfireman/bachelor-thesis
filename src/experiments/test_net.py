d5cc64f61a8261ca51174718f9d0c1033ca5b0d2
Ture Claussen <ture.cl.business@gmail.com>
2021-11-09 13:32:01 +0100

    Adapt to dotdict api

--- src/experiments/test_net.py
+++ src/experiments/test_net.py
@@ -3,17 +3,18 @@ import sys
 sys.path.append('..')
 sys.path.append('../..')
 
+from alpha_zero_general.utils import dotdict
 from src.Abalone import AbaloneGame
 from src.AbaloneNN import AbaloneNN
 
-nn = AbaloneNN(AbaloneGame(), {
+nn = AbaloneNN(AbaloneGame(), dotdict({
     'lr': 0.001,
     'dropout': 0.3,
     'epochs': 10,
     'batch_size': 64,
     'cuda': False,
     'num_channels': 512,
-})
+}))
 
 nn.show_info()
 nn.visualize()
