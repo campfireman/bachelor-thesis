c2b01e7b9ed041a19df08aef62cf1425d1d89fa4
Ture Claussen <ture.cl.business@gmail.com>
2021-11-18 09:21:46 +0100

    Update data type

--- code/src/abalone_game.py
+++ code/src/abalone_game.py
@@ -55,7 +55,7 @@ class AbaloneGame(Game):
             [0, 0, 1, 1, 1, 0, 0, 0, 0],  # 6
             [1, 1, 1, 1, 1, 1, 0, 0, 0],  # 7
             [1, 1, 1, 1, 1, 0, 0, 0, 0],  # 8
-        ], dtype='int')
+        ], dtype='int8')
 
     def getBoardSize(self):
         """
