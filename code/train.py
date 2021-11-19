d8b91f36a0416fb7d53e40af24657478f86f7994
Ture Claussen <ture.cl.business@gmail.com>
2021-11-19 14:38:06 +0100

    Use dataclass instead of dotdict

--- code/train.py
+++ code/train.py
@@ -1,39 +1,46 @@
 import logging
 import os
+from dataclasses import dataclass
+from typing import Tuple
 
 import coloredlogs
-from alpha_zero_general.utils import dotdict
 
 from src.abalone_game import AbaloneGame as Game
-from src.coach import Coach
+from src.coach import ParallelCoach as Coach
+# from src.coach import Coach
 from src.neural_net import NNetWrapper as nn
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 log = logging.getLogger(__name__)
 
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 
-args = dotdict({
-    'numIters': 1000,
-    # Number of complete self-play games to simulate during a new iteration.
-    'numEps': 20,
-    'tempThreshold': 15,        #
+
+@dataclass
+class TrainingArguments:
+    numIters: int = 1000
+    numEps: int = 10
+    tempThreshold: int = 15
     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'updateThreshold': 0.6,
+    updateThreshold: float = 0.6
     # Number of game examples to train the neural networks.
-    'maxlenOfQueue': 200000,
-    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    maxlenOfQueue: int = 2000000
+    # Number of games moves for MCTS to simulate.
+    numMCTSSims: int = 2
     # Number of games to play during arena play to determine if new net will be accepted.
-    'arenaCompare': 6,
-    'cpuct': 1,
+    arenaCompare: int = 6
+    cpuct: float = 1
+    n_self_play_workers: int = 4
 
-    'checkpoint': './tempX/',
-    'load_model': False,
-    'load_folder_file': ('/home/ture/projects/bachelor-thesis/src/temp', 'temp.pth.tar'),
-    'numItersForTrainExamplesHistory': 20,
+    checkpoint: str = './temp/'
+    load_model: bool = False
+    load_folder_file: Tuple[str, str] = (
+        '/home/ture/projects/bachelor-thesis/code/src/temp', 'temp.pth.tar')
+    numItersForTrainExamplesHistory: int = 20
 
-})
+
+args = TrainingArguments()
 
 
 def main():
@@ -62,4 +69,6 @@ def main():
 
 
 if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.set_start_method('spawn')
     main()
