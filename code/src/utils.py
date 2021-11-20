ef78acfa86de33d2d7cef983877319750a0cbb69
Ture Claussen <ture.cl.business@gmail.com>
2021-11-21 00:27:25 +0100

    Add class for saving csv tables

--- code/src/utils.py
+++ code/src/utils.py
@@ -1,3 +1,8 @@
+import csv
+import os
+from ctypes import ArgumentError
+from typing import List
+
 from .experiments.possible_moves import POSSIBLE_MOVES
 
 
@@ -7,3 +12,20 @@ def move_index_to_standard(index: int) -> str:
 
 def move_standard_to_index(move: str) -> int:
     return POSSIBLE_MOVES[move]
+
+
+class CsvTable:
+    def __init__(self, path: str, filename: str, header: List):
+        self.file = open(os.path.join(path, filename), 'w')
+        self.csv_writer = csv.writer(self.file, delimiter=',')
+        self.header = header
+        self.csv_writer.writerow(self.header)
+
+    def add_row(self, row: List):
+        if len(row) != len(self.header):
+            raise ArgumentError(
+                f'Incorrect dimension of row, header has {len(self.header)} and row has {len(row)}')
+        self.csv_writer.writerow(row)
+
+    def __del__(self):
+        self.file.close()
