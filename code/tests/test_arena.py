9405c3f3157fb4c8d7f76363302837d63a768e3e
Ture Claussen <ture.cl.business@gmail.com>
2021-12-03 11:58:53 +0100

    Adapt to switch to pytorch

--- code/tests/test_arena.py
+++ code/tests/test_arena.py
@@ -8,12 +8,12 @@ from src.settings import CoachArguments
 
 def test_play_games():
     test_net_path = os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), 'lib/nnet.tar')
+        os.path.realpath(__file__)), 'lib/best.pth.tar')
     arena = ParallelArena(
         AbaloneNNPlayer,
         (),
-        {'nnet_fullpath': os.path.join(os.path.dirname(
-            __file__), 'lib/temp.pth.tar'), 'args': CoachArguments(num_MCTS_sims=4)},
+        {'nnet_fullpath': test_net_path,
+            'args': CoachArguments(num_MCTS_sims=1, nnet_size='small', framework='torch')},
         RandomPlayer,
         (),
         {},
