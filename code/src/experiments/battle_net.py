0808e8800472bec6f4bbf40217dba35aeecbe4a2
Ture Claussen <ture.cl.business@gmail.com>
2021-12-07 10:16:01 +0100

    Fix network name

--- code/src/experiments/battle_net.py
+++ code/src/experiments/battle_net.py
@@ -17,7 +17,7 @@ def main():
     # black = AbaProPlayer(Player.BLACK, is_verbose=False, depth=3)
     black = RandomPlayer(Player.BLACK)
     white = AbaloneNNPlayer(Player.WHITE, '/home/ture/projects/bachelor-thesis/code/data/temp/heuristic_warm_up_net.pth.tar',
-                            CoachArguments(framework='pytorch', nnet_size='mini', cuda=True, num_MCTS_sims=60))
+                            CoachArguments(framework='torch', nnet_size='mini', cuda=True, num_MCTS_sims=60))
     # white = AbaProPlayer(Player.WHITE, is_verbose=False, depth=2)
     game, moves_history, move_stats = Game.run_game(
         black, white, is_verbose=True)
