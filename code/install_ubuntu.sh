72991f7c3060abd4ffc412332a6afb0da9030fd1
Ture Claussen <ture.cl.business@gmail.com>
2021-12-12 01:59:36 +0100

    Add compute server setup script

---
+++ code/install_ubuntu.sh
@@ -0,0 +1,16 @@
+git clone git@github.com:campfireman/bachelor-thesis.git
+cd bachelor-thesis/code
+sudo apt update
+sudo apt install python3-pip
+pip3 install numpy
+pip3 install Cython
+pip3 install -r requirements.txt
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda-repo-ubuntu2004-11-3-local_11.3.0-465.19.01-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-3-local_11.3.0-465.19.01-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-3-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+nohup python3 train.py --args prod.json > output.txt &
+
